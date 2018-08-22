4ec3ec44416c8441fa5116d78249095bca16486f
thijs <thijs@go2people.nl>
2018-08-22 13:25:56 +0200

    remove mptt.level, make slug field nullable

--- mezzanine_blocks/category.py
+++ mezzanine_blocks/category.py
@@ -1,29 +1,15 @@
 from django.db import models
-from django.core.cache import cache
 from django.utils.translation import ugettext_lazy as _
 from mezzanine.core.models import Slugged
 
 
-try:
-    from mptt.models import MPTTModel, TreeForeignKey
+class BaseBlockCategory(Slugged):
+    """Base Category"""
 
-    class BaseBlockCategory(MPTTModel, Slugged):
-        """Base Category (MPTT tree).
-        """
-        parent = TreeForeignKey('self', null=True, blank=True, related_name='child')
+    slug = models.CharField(max_length=2000, blank=True, null=True)
 
-        class Meta:
-            abstract = True
-
-        class MPTTMeta:
-            order_insertion_by = ['title']
-
-except ImportError:
-    class BaseBlockCategory(Slugged):
-        """Base Category
-        """
-        class Meta:
-            abstract = True
+    class Meta:
+        abstract = True
 
 
 class BlockCategory(BaseBlockCategory):
