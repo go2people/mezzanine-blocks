9a0806c802bc3148dd2637c6d18e22aa871ec948
Joep Boekhold <joepboekhold@gmail.com>
2020-03-18 09:57:07 +0100

    Updated to support Django 2.2

--- mezzanine_blocks/migrations/0001_initial.py
+++ mezzanine_blocks/migrations/0001_initial.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
+import django.db.models.deletion
 import mezzanine.core.fields
 import mezzanine.utils.models
 
@@ -34,7 +35,7 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('title', models.CharField(max_length=500, verbose_name='Title')),
                 ('slug', models.CharField(help_text='Leave blank to have the URL auto-generated from the title.', max_length=2000, null=True, verbose_name='URL', blank=True)),
-                ('site', models.ForeignKey(editable=False, to='sites.Site')),
+                ('site', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, editable=False, to='sites.Site')),
             ],
             options={
                 'verbose_name': 'Block category',
@@ -55,8 +56,8 @@ class Migration(migrations.Migration):
                 ('height', models.IntegerField(default=100, help_text='Height in pixels.', verbose_name='Height')),
                 ('width', models.IntegerField(default=200, help_text='Width in pixels.', verbose_name='Width')),
                 ('quality', models.IntegerField(default=80, verbose_name='Quality')),
-                ('category', models.ForeignKey(blank=True, to='mezzanine_blocks.BlockCategory', null=True)),
-                ('site', models.ForeignKey(editable=False, to='sites.Site')),
+                ('category', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, blank=True, to='mezzanine_blocks.BlockCategory', null=True)),
+                ('site', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, editable=False, to='sites.Site')),
             ],
             options={
                 'verbose_name': 'Image Block',
@@ -73,8 +74,8 @@ class Migration(migrations.Migration):
                 ('content', mezzanine.core.fields.RichTextField(verbose_name='Content')),
                 ('login_required', models.BooleanField(default=False, help_text='If checked, only logged in users can view this page', verbose_name='Login required')),
                 ('show_title', models.BooleanField(default=False, help_text='If checked, show block title', verbose_name='Show title')),
-                ('category', models.ForeignKey(blank=True, to='mezzanine_blocks.BlockCategory', null=True)),
-                ('site', models.ForeignKey(editable=False, to='sites.Site')),
+                ('category', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, blank=True, to='mezzanine_blocks.BlockCategory', null=True)),
+                ('site', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, editable=False, to='sites.Site')),
             ],
             options={
                 'verbose_name': 'Rich Block',
@@ -84,11 +85,11 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='block',
             name='category',
-            field=models.ForeignKey(blank=True, to='mezzanine_blocks.BlockCategory', null=True),
+            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, blank=True, to='mezzanine_blocks.BlockCategory', null=True),
         ),
         migrations.AddField(
             model_name='block',
             name='site',
-            field=models.ForeignKey(editable=False, to='sites.Site'),
+            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, editable=False, to='sites.Site'),
         ),
     ]
--- mezzanine_blocks/models.py
+++ mezzanine_blocks/models.py
@@ -12,7 +12,7 @@ class BaseBlock(Slugged):
     """Base Block
     """
     slug = models.CharField(max_length=2000, blank=True, null=True)
-    category = models.ForeignKey(BlockCategory, null=True, blank=True)
+    category = models.ForeignKey(BlockCategory, null=True, blank=True, on_delete=models.CASCADE)
     login_required = models.BooleanField(_("Login required"), help_text=_("If checked, only logged in users can view this page"), default=False)
     show_title = models.BooleanField(_("Show title"), help_text=_("If checked, show block title"), default=False)
 
